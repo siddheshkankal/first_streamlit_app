24c622a9122b97f680f046a994bde4da55d2f006
Siddhesh Kankal <60224016+siddheshkankal@users.noreply.github.com>
2023-04-08 22:12:46 +0530

    Update streamlit_app.py
    adding if else statement

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,21 +32,22 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  if not fruit_choise:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    streamlit.write('The user entered ', fruit_choice)
 
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-
-
-# streamlit.text(fruityvice_response.json())
-
-
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    # streamlit.text(fruityvice_response.json())
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    # write your own comment - what does this do?
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
