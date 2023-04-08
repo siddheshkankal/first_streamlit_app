483b563fb7ba530a7da642d153d0802d1dca285e
Siddhesh Kankal <60224016+siddheshkankal@users.noreply.github.com>
2023-04-08 11:26:13 +0530

    Update streamlit_app.py
    added some menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner ')
 
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 and Blueberry meal')
+streamlit.text('Aloo paratha')
+streamlit.text('Hard boiled egg')
+streamlit.text('Chicken noodles')
+
